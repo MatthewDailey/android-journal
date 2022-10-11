d120d25c94d40ba9ab8f2291cdfb31727033f7a4
Matt Dailey <mjd@figma.com>
2022-10-11 09:26:44 -0700

    add PM test

--- components/list_item.test.tsx
+++ components/list_item.test.tsx
@@ -5,11 +5,20 @@ import { LightText, NormalText } from "./text"
 
 describe('List Items', () => {
     describe('GratitudeListItem', () => {
-        it('presents date - AM', () => {
+        it('presents date - PM', () => {
             const tree: any = renderer.create(<GratitudeListItem text="amazing wife" dateMs={1665553993440} />)
             const header = tree.root.findByType(LightText)
             expect(header).toBeDefined()
             expect(header.props.children).toContain('10:53pm')
+            const text = tree.root.findByType(NormalText)
+            expect(text).toBeDefined()
+            expect(text.props.children).toEqual("amazing wife")
+        })
+        it('presents date - AM', () => {
+            const tree: any = renderer.create(<GratitudeListItem text="amazing wife" dateMs={1665503993440} />)
+            const header = tree.root.findByType(LightText)
+            expect(header).toBeDefined()
+            expect(header.props.children).toContain('8:59am')
             const text = tree.root.findByType(NormalText)
             expect(text).toBeDefined()
             expect(text.props.children).toEqual("amazing wife")
