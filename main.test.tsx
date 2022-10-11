c1a45921d782cf6191c064cef9c4e7b2e74e7472
Matt Dailey <mjd@figma.com>
2022-10-11 08:41:46 -0700

    remove unused test

--- main.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react"
-import renderer from 'react-test-renderer'
-import { MainView } from "./main"
-
-describe('MainView', () => {
-    it('passes', () => {
-      
-    })
-})
