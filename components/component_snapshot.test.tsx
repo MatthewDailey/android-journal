50bc89abc6dc497ebe89fa46914cf4a9c21ad37e
Matt Dailey <mjd@figma.com>
2022-10-11 13:56:21 -0700

    add snapshot tests for list items

--- components/component_snapshot.test.tsx
+++ components/component_snapshot.test.tsx
@@ -1,7 +1,8 @@
 import React from "react"
 import renderer from 'react-test-renderer'
 import { ButtonContainer, PrimaryButton } from "./button"
-import { HeavyText, LightText, NormalText } from "./text"
+import { DateListItem, EditingListItem, GratitudeListItem, JournalListItem, NoEntryListItem } from "./list_items"
+import { HeavyText, LightText, NormalText, NormalTextInput } from "./text"
 
 describe('Component Snapshot Tests', () => {
     it('PrimaryButton', () => {    
@@ -27,6 +28,30 @@ describe('Component Snapshot Tests', () => {
     })
     it('NormalText', () => {
         const tree: any = renderer.create(<NormalText>Exerted enterence focus hear him</NormalText>).toJSON()
+        expect(tree).toMatchSnapshot()
+    })
+    it('NormalTextInput', () => {
+        const tree = renderer.create(<NormalTextInput />).toJSON()
+        expect(tree).toMatchSnapshot()
+    })
+    it('EditingListItem', () => {
+        const tree = renderer.create(<EditingListItem header="I'm grateful for..." onChangeText={function (text: string): void {} } />).toJSON()
+        expect(tree).toMatchSnapshot()
+    })
+    it('GratitudeListItem', () => {
+        const tree = renderer.create(<GratitudeListItem text="This is a long string that will wrap to many lines that needs to be truncated so that this element does not take up too much space." dateMs={1665553993440} />).toJSON()
+        expect(tree).toMatchSnapshot()
+    })
+    it('JournalListItem', () => {
+        const tree = renderer.create(<JournalListItem text="This is a long string that will wrap to many lines that needs to be truncated so that this element does not take up too much space." dateMs={1665553993440} />).toJSON()
+        expect(tree).toMatchSnapshot()
+    })
+    it('DateListItem', () => {
+        const tree = renderer.create(<DateListItem dateMs={new Date('2022-10-10T20:12:30.913Z').getTime()}/>).toJSON()
+        expect(tree).toMatchSnapshot()
+    })
+    it('NoEntryListItem', () => {
+        const tree = renderer.create(<NoEntryListItem />).toJSON()
         expect(tree).toMatchSnapshot()
     })
 })
