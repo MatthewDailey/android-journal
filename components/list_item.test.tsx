411177ff91988566a4fcf2297acb7f0d7ada9346
Matt Dailey <mjd@figma.com>
2022-10-11 09:24:55 -0700

    unit test for gratitude item that passes

---
+++ components/list_item.test.tsx
@@ -0,0 +1,18 @@
+import React from "react"
+import renderer from 'react-test-renderer'
+import { GratitudeListItem } from "./list_items"
+import { LightText, NormalText } from "./text"
+
+describe('List Items', () => {
+    describe('GratitudeListItem', () => {
+        it('presents date - AM', () => {
+            const tree: any = renderer.create(<GratitudeListItem text="amazing wife" dateMs={1665553993440} />)
+            const header = tree.root.findByType(LightText)
+            expect(header).toBeDefined()
+            expect(header.props.children).toContain('10:53pm')
+            const text = tree.root.findByType(NormalText)
+            expect(text).toBeDefined()
+            expect(text.props.children).toEqual("amazing wife")
+        })
+    })
+})
