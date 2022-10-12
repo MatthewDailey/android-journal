423f028d093128bb7a1df274a336ecf96b1363ad
Matt Dailey <mjd@figma.com>
2022-10-12 07:53:41 -0700

    add basic list items

--- components/scrolling_list.test.tsx
+++ components/scrolling_list.test.tsx
@@ -3,10 +3,12 @@ import { render, screen } from '@testing-library/react-native'
 import renderer from 'react-test-renderer'
 import { ScrollingList } from './scrolling_list'
 import { FlatList } from 'react-native'
+import { DateListItem, NoEntryListItem } from './list_items'
 
 describe('ScrollingList', () => {
-    it('renders', () => {
+    it('empty entries includes today and no entry', () => {
         const tree = renderer.create(<ScrollingList entries={[]} />)
-        expect(tree.root.findByType(FlatList)).toBeTruthy()
+        expect(tree.root.findByType(NoEntryListItem)).toBeTruthy()
+        expect(tree.root.findByType(DateListItem).findByProps({ children: "Today" })).toBeTruthy()
     })
 })
--- components/scrolling_list.tsx
+++ components/scrolling_list.tsx
@@ -1,17 +1,38 @@
 import React from 'react'
 import { FlatList, StyleSheet, Text, View } from 'react-native'
 import { JournalEntry } from "../types";
+import { DateListItem, EditingListItem, GratitudeListItem, JournalListItem, NoEntryListItem } from './list_items';
+
+type ListItemType = {
+    type: 'date',
+    dateMs: number
+} | {
+    type: 'no_entry',
+} | {
+    type: 'entry',
+    entryData: JournalEntry,
+}
+
 
 export const ScrollingList = (props: { entries: JournalEntry[] }) => {
-    const renderItem = () => {
-        return <View>
-            <Text>Item</Text>
-        </View>
+    const renderItem = ({item} : {item: ListItemType}) => {
+        switch(item.type) {
+            case 'date':
+                return <DateListItem dateMs={item.dateMs} />
+            case 'no_entry':
+                return <NoEntryListItem />
+            default:
+                return null
+        }
     }
+
+    const entryListToRender: ListItemType[] = []
+    entryListToRender.push({ type: 'date', dateMs: new Date().getTime() })
+    entryListToRender.push({ type: 'no_entry' })
 
     return (
         <FlatList 
-            data={props.entries}
+            data={entryListToRender}
             renderItem={renderItem}
         />
     )
